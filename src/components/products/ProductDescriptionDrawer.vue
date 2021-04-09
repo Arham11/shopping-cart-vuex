2fb45f5863d4ab7dabf49b274f9017e26ab98d76
arham chowdhury <arham.chowdhury@paytm.com>
2021-04-09 10:07:59 +0530

    product modal/drawer ui changes

--- src/components/products/ProductDescriptionDrawer.vue
+++ src/components/products/ProductDescriptionDrawer.vue
@@ -1,14 +1,40 @@
 <template>
-  <div class="product-drawer fixed top-0 bottom-0 p-4 bg-white">
-    <div class="text-right mb-4">
-      <span
-        class="bg-gray-300 leading-none rounded-full px-2 py-1 text-white justify-items-end cursor-pointer"
-        @click="close"
-        >x</span
-      >
-    </div>
-    <div>
-      {{ product.description }}
+  <div class="product-drawer fixed inset-0 p-4 bg-white">
+    <div class="w-1/2 mx-auto">
+      <div class="text-right mb-4 -mx-10">
+        <span
+          class="bg-gray-300 leading-none rounded-full px-2 py-1 text-white justify-items-end cursor-pointer"
+          @click="close"
+          >x</span
+        >
+      </div>
+      <div class="text-lg mb-4 font-bold">
+        {{ product.title }}
+      </div>
+      <div class="mb-4 text-base">
+        {{ product.description }}
+      </div>
+      <div class="mb-5">
+        <span>Price : </span>
+        <span class="text-red-700">$ {{ product.price }}</span>
+      </div>
+      <div class="text-base mb-4 font-semibold">
+        In Cart (0)
+      </div>
+      <div>
+        <button
+          @click="veiwDetails"
+          class="bg-gray-400 py-1 px-2 text-white font-bold rounded-md px-7 mr-3"
+        >
+          +
+        </button>
+        <button
+          @click="veiwDetails"
+          class="bg-gray-400 py-1 px-2 text-white font-bold rounded-md px-7 ml-3"
+        >
+          -
+        </button>
+      </div>
     </div>
   </div>
 </template>
